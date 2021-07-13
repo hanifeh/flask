034b0f4e9805ac2bf09a0a0ce3c785c7f3867c2f
sadegh <sadeghhanifehnia@gmail.com>
2021-07-13 19:59:03 +0430

    start flask

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'get'])
 def hello_world():
-    return 'Hello World!'
+    # return 'Hello World!'
+    return render_template('hello.html', name='sadegh')
+
+
+@app.route('/hello/<name>/')
+def hello(name):
+    return f'Hello {escape(name)}'
+
+
+@app.route('/num/<int:pk>/')
+def hello_number(pk):
+    return f'Hello {escape(pk)}'
 
 
 if __name__ == '__main__':

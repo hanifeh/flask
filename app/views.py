bd9ae3ee714c99cfd7708d0d2a692172c3222165
sadegh <sadeghhanifehnia@gmail.com>
2021-07-15 17:08:40 +0430

    add mongodb

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
+from flask_pymongo import PyMongo
+
 from . import app
 
+app.config['MONGO_URI'] = 'mongodb://maktab:123456@localhost:27017/maktab?authSource=admin'
+mongo = PyMongo(app)
 
+
+@app.route('/all')
+def show_all():
+    all_docs = mongo.db.flask.find()
+    print(all_docs)
+    return all_docs[0]['name']
